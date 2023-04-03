e6ef58922a24dcd04cf989651d55128336115623
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-03 18:25:04 -0300

    atualização

---
+++ siteVerificadorIdade/script.js
@@ -0,0 +1,14 @@
+function carregar(){
+    let ano = document.querySelector('input#ano')
+    let homem = document.querySelector('input#homem')
+    let mulher = document.querySelector('input#mulher')
+    let resultado = document.querySelector('p#resultado')
+    let img = document.querySelector('img#imagem')
+    let data = new Date()
+    let hoje = data.getFullYear
+if (hoje-ano > 18){
+    resultado.innerHTML = 'Homem'
+} else{
+
+}
+}
