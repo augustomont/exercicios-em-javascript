e04296258220c8308c7a4a2cde099ff46895c36e
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-03 20:15:13 -0300

    reestruturando

--- siteVerificadorIdade/script.js
+++ siteVerificadorIdade/script.js
@@ -1,4 +1,31 @@
-function carregar(){
+function verificar(){
+    let data = new Date()
+    let ano = data.getFullYear()
+    let fano = document.querySelector('input#txtano')
+    let res = document.querySelector('div#res')
+
+    if (fano.value.length == 0 || fano.value > ano){
+        window.alert('erro')
+    } else{
+        let fsex = document.getElementsByName('radsex')
+        let idade =  ano - Number(fano.value) 
+        let genero = '' 
+        if(fsex[0].checked){
+            genero = 'Homem'
+        } else if(fsex[1].checked){
+            genero = 'Mulher'
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade}`
+    }
+}
+
+
+
+
+
+
+
+/*function verificar(){
     let ano = document.querySelector('input#ano')
     let homem = document.querySelector('input#homem')
     let mulher = document.querySelector('input#mulher')
@@ -12,4 +39,4 @@ if (hoje -= ano > 18){
 } else{
 
 }
-}
+}*/
