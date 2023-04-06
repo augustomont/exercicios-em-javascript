5500df4686f66cca140ff844087ce4c637dd5437
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-06 18:08:14 -0300

    Update script.js

--- siteContador/script.js
+++ siteContador/script.js
@@ -8,10 +8,16 @@ function contar(){
         window.alert('preencha')
     } else {
         res.innerHTML = 'Contando......'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+
+         for(let c = i;c<=f;c+=p){
+    res.innerHTML += `${c}&#128516`
+   }
+
     }
 
     
-    /*for(ini;ini<=fim;ini+pas){
-    window.alert(ini)
-   }*/
+   
 }
