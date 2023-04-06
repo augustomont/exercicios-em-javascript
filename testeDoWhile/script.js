874ad8bdd22c18744ab1a6cc8e27a9d0d9591614
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-06 14:14:28 -0300

    atualização

--- testeDoWhile/script.js
+++ testeDoWhile/script.js
@@ -1,8 +1,15 @@
 let c = 1
 let escre = document.querySelector('p#escr')
-
+let writ = document.querySelector('p#write')
 function escrever(){
     while (c<6){
         escre.innerHTML = `Olá ${c}`
         c++
     } }
+
+
+function wri(){
+    for (c;c<10;c++){
+       writ.innerHTML = `Oi ${c}`
+    }
+}
