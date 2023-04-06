38b3ba288b911719f5bcd963ebdc0ca52918b283
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-06 18:19:28 -0300

    Update script.js

--- siteContador/script.js
+++ siteContador/script.js
@@ -11,11 +11,16 @@ function contar(){
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(pas.value)
-
-         for(let c = i;c<=f;c+=p){
-    res.innerHTML += `${c}&#128516`
-   }
-
+        
+        if(i<f){
+            for(let c = i;c<=f;c+=p){
+                res.innerHTML += `${c} &#128516 `
+      }
+        } else {
+            for(let c = i;c>=f;c-=p){
+                res.innerHTML += `${c} &#128516 `
+            }
+        }
     }
 
     
