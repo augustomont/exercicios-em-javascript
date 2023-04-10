6a3022e60b261b4653e508072866cae2bcb04882
Augusto Monteiro <augustoengenharia@outlook.com>
2023-04-10 14:00:19 -0300

    Update functions.js

--- functions.js
+++ functions.js
@@ -5,10 +5,10 @@ function parimp(n){
         return `${n} é impar`
     }
 }
-
 let num = parimp(2)
 console.log(num)
 
+/*função para saber o fatorial de um numero*/
 function fatorial(nu){
     let fat = 1
     for(c = nu;c>1;c--){
@@ -16,7 +16,17 @@ function fatorial(nu){
     }
     return fat
 }
-
 let nu = 5
-
 console.log(fatorial(nu))
+
+/*Tambem podemos fazer de forma recursiva*/
+
+function fator(a){
+    if(a<=1){
+        return 1
+    } else{
+      return  a * fator(a-1)
+    }
+
+}
+console.log(fator(6))
